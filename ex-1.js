4d87e4a306be7a2b82be780dcf6e3e69871d5773
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-10-01 10:01:31 +0500

    I have completed 2nd exercise of The Exercises with JavaScript and Node.js.

--- ex-1.js
+++ ex-1.js
@@ -2,7 +2,9 @@
 
 // Store a person’s name in a variable
 
-let message = "Hello Eric, would you like to learn some Javascript today?"
+let person_name = "Eric"
+
+let message = `Hello ${person_name}, would you like to learn some Python today?`
 
 // and print a message to that person. Your message should be simple,
 //  such as, “Hello Eric, would you like to learn some Python today?”

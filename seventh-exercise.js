51302701c2275238e244e563c8ae03c338a9c740
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-10-01 11:31:35 +0500

    I have completed 8th exercise of The Exercises with JavaScript and Node.js.

---
+++ seventh-exercise.js
@@ -0,0 +1,8 @@
+// 7: Favorite Number:
+//  Store your favorite number in a variable. 
+// Then, using that variable, create a message 
+// that reveals your favorite number. Print that message.
+
+let favorite_number = 11
+
+console.log('my favorite_number number is ', favorite_number);

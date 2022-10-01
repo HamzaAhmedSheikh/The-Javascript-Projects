f5395ede104a2ee7401870398ee7733b6a9786fc
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-10-01 10:11:12 +0500

    I have completed 3rd exercise of The Exercises with JavaScript and Node.js.

---
+++ third-exercise.js
@@ -0,0 +1,11 @@
+// 4: Famous Quote: Find a quote from a famous person you admire.
+//  Print the quote and the name of its author.
+//  Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+//  Print the quote and the name of its author.
+let famous_person = 'Albert Einstein'
+let famous_person_quote = "once said a person who never made a mistake never tried anything new."
+
+console.log(` the quote is:- ${famous_person} ${famous_person_quote}`)

3fadcb346a49bfe506499ce084d92cd7b2a28948
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-10-01 09:34:45 +0500

    I have completed 1st exercise of Exercises with JavaScript and Node.js.

---
+++ ex-1.js
@@ -0,0 +1,10 @@
+// 2: Personal Message: 
+
+// Store a person’s name in a variable
+
+let message = "Hello Eric, would you like to learn some Javascript today?"
+
+// and print a message to that person. Your message should be simple,
+//  such as, “Hello Eric, would you like to learn some Python today?”
+
+console.log('message ==> ', message);

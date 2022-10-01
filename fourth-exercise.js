bd4622712369bf0d109713a2930fb13488f5131c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-10-01 10:23:13 +0500

    I have completed 4th exercise of The Exercises with JavaScript and Node.js and update third-exercise.js

---
+++ fourth-exercise.js
@@ -0,0 +1,9 @@
+// Famous Quote 2: Repeat Exercise 3, but this time store 
+// the famous person’s name in a variable called famous_person.
+//  Then compose your message and store it in a new variable called message. 
+// Print your message.
+
+let famous_person = 'Albert Einstein'
+let famous_person_quote = "once said a person who never made a mistake never tried anything new."
+
+console.log(` the quote is:- ${famous_person} ${famous_person_quote}`)
--- third-exercise.js
+++ third-exercise.js
@@ -5,7 +5,7 @@
 
 
 //  Print the quote and the name of its author.
-let famous_person = 'Albert Einstein'
-let famous_person_quote = "once said a person who never made a mistake never tried anything new."
+let quote = 'a person who never made a mistake never tried anything new.'
 
-console.log(` the quote is:- ${famous_person} ${famous_person_quote}`)
+console.log(`Albert Einstein once said ${quote}`)
+
